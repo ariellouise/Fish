68064a440a22a26f3cdead16ecb7e22eb430050d
ariel murphy <arielladders@gmail.com>
2020-04-29 04:40:15 +0000

    modified episodes

--- lib/episodes.rb
+++ lib/episodes.rb
@@ -1,9 +1,19 @@
 
 
 class Playlists
+ 
 
 end 
 
+
+
 class Episodes
+  
+  attr_accessor :title, :url
+  
+  def initialize(title, url)
+    @title, @url = title, url
+    @@all << self
+  end
   
 end
