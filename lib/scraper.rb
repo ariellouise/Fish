49ac34b8e75d518a9d75f01ff46a3258c19c990a
ariel murphy <arielladders@gmail.com>
2020-04-29 02:09:13 +0000

    modified scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,21 @@
-require 'open=uri'
+require 'open-uri'
+require 'nokogiri'
 
 
 base_url = "https://soundcloud.com/fishguymedia/sets"
-sports_url = "https://soundcloud.com/fishguymedia/sets/up-next-sports"
-comedy_url = "https://soundcloud.com/fishguymedia/sets/comics-of-the-round-table"
-fishbowl_url = "https://soundcloud.com/fishguymedia/sets/fish-bowl-podcast"
-32bit_url = "https://soundcloud.com/fishguymedia/sets/32-bit-arena-podcast"
-uptown_url = "https://soundcloud.com/fishguymedia/sets/uptown-series"
+#sports_url = "https://soundcloud.com/fishguymedia/sets/up-next-sports"
+#comedy_url = "https://soundcloud.com/fishguymedia/sets/comics-of-the-round-table"
+#fishbowl_url = "https://soundcloud.com/fishguymedia/sets/fish-bowl-podcast"
+#bit_url = "https://soundcloud.com/fishguymedia/sets/32-bit-arena-podcast"
+#uptown_url = "https://soundcloud.com/fishguymedia/sets/uptown-series"
 
 
+page = open(base_url)
 
-class Scraper
+puts page.class #tempfile
+puts page.content_type #text/html
+#puts page.read 
+puts page.read.class #string
+
+html_text = page.read
   
