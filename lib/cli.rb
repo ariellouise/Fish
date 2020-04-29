675a95b1071237c9036b761c53265f2a941ad558
ariel murphy <arielladders@gmail.com>
2020-04-29 05:15:43 +0000

    modified cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -40,10 +40,12 @@ def list_uptown
   
 def menu
   input_1 = ""
-  input = ""
   puts "For a list of episodes of the show, *Up Next Sports Show!*, press '1'."
   puts "For a list of episodes of the show, *Comics Of The Round Table*, press '2'."
   puts "For a list of episodes of the show, *The Fishbowl Podcast*, press '3'."
   puts "For a list of episodes of the show, *32 Bit Arena! Podcast*, press '4'."
   puts "For a list of episodes of the show, *Uptown Series Podcast*, press '5'."
+  puts "To end the application, type 'exit'."
+  input_1 = gets.strip
+  
   
