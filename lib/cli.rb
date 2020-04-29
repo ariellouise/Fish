fcd537cd2132051549b31bc44e29b0a370d2f145
ariel murphy <arielladders@gmail.com>
2020-04-29 05:07:31 +0000

    modified cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,12 +7,31 @@ class CLI
 end
 
 def list_sports
+  @episodes
+  @episodes_sports
+
+
 
 def list_comedy
+  @episodes
+  @episodes_comedy
+  
+  
+  
   
 def list_fishbowl
+  @episodes 
+  @episodes_fishbowl
+  
+  
   
 def list_bit
+  @episodes 
+  @episodes_bit
+  
+  
   
 def list_uptown
+  @episodes
+  @episodes_uptown
   
