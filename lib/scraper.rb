7a09181c8e7e4f11671903e3d1aa373d53d2f850
ariel murphy <arielladders@gmail.com>
2020-05-01 21:10:11 +0000

    modified scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,21 +6,21 @@ require 'nokogiri'
 
 base_url = "https://soundcloud.com/fishguymedia/sets"
 #sports_url = "https://soundcloud.com/fishguymedia/sets/up-next-sports"
-#comedy_url = "https://soundcloud.com/fishguymedia/sets/comics-of-the-round-table"
+comedy_url = "https://soundcloud.com/fishguymedia/sets/comics-of-the-round-table"
 #fishbowl_url = "https://soundcloud.com/fishguymedia/sets/fish-bowl-podcast"
 #bit_url = "https://soundcloud.com/fishguymedia/sets/32-bit-arena-podcast"
 #uptown_url = "https://soundcloud.com/fishguymedia/sets/uptown-series"
 
 
-base_page = open(base_url)
-base_html = base_page.read
+#base_page = open(base_url)
+#base_html = base_page.read
 
 
 #sports_page = open(sports_url)
 #sports_html = sports_page.read
 
-#comedy_page = open(comedy_url)
-#comedy_html = comedy_page.read
+comedy_page = open(comedy_url)
+comedy_html = comedy_page.read
 
 #fishbowl_page = open(fishbowl_url)
 #fishbowl_html = fishbowl_page.read
@@ -31,18 +31,18 @@ base_html = base_page.read
 #uptown_page = open(uptown_url)
 #uptown_html = uptown_page.read
 
-parsed_base = Nokogiri::HTML(base_html)
+#parsed_base = Nokogiri::HTML(base_html)
 #parsed_sports = Nokogiri::HTML(sports_html)
-#parsed_comedy = Nokogiri::HTML(comedy_html)
+parsed_comedy = Nokogiri::HTML(comedy_html)
 #parsed_fishbowl = Nokogiri::HTML(fishbowl_html)
 #parsed_bit = Nokogiri::HTML(bit_html)
 #parsed_uptown = Nokogiri::HTML(uptown_html)
 
 #puts parsed_base.class
 
-base_episodes = parsed_base.css("")
+#base_episodes = parsed_base.css("")
 #sports_episodes = parsed_sports.css("")
-#comedy_episodes = parsed_comedy.css("")
+comedy_episodes = parsed_comedy.css("#content div.l-listen-wrapper div.l-about-main div.l-about-row.l-listen__mainContent div.l-about-right div.listenDetails__trackList ul li:nth-child(3) div.trackItem__content.sc-truncate a")
 #fishbowl_episodes = parsed_fishbowl.css("")
 #bit_episodes = parsed_bit.css("")
 #uptown_episodes = parsed_uptown.css("")
